5054f836b95e6d8f945ad56e1498cbc71e5afa95
masao takayama <m.takayama0929@gmail.com>
2020-10-18 12:41:04 +0000

    day3ペアワークの成果を提出します。

---
+++ script.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('.btn').on('mouseenter', function(){
+    $(this).css('background-color', '#873955');
+  })
+  .on('mouseleave', function(){
+    $(this).css('background-color', '#0074bf');
+  });
+});
