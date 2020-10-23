458f90de8414cff5261945a7fb0eb9f947870ca0
masao takayama <65898039+enjoy-self-inquiry@users.noreply.github.com>
2020-10-23 19:33:26 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  $('.btn').on('mouseenter', function(){
-    $(this).css('background-color', '#873955');
-  })
-  .on('mouseleave', function(){
-    $(this).css('background-color', '#0074bf');
-  });
-});
